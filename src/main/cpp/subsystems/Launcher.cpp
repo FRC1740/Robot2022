b881fd9791e6a7ab2c5ad5128203c103eb9b88e9
Charles Estabrooks <charles@estabrooks.org>
2022-02-19 11:03:58 -0500

    Added Vision subsystem

--- src/main/cpp/subsystems/Launcher.cpp
+++ src/main/cpp/subsystems/Launcher.cpp
@@ -37,96 +37,121 @@ Launcher::Launcher() {
 
             m_sbt_Launcher = &frc::Shuffleboard::GetTab(ConShuffleboard::LauncherTab);
 
-            m_nte_Ernie_FwdLimit = m_sbt_Launcher->AddPersistent("Ernie Fwd Limit", ConLauncher::ERNIE_FWD_LIMIT)
+            // Robot outputs -> Shuffleboard
+            m_nte_Bert_Position = m_sbt_Launcher->AddPersistent("Bert Pos", ConLauncher::BERT_REV_LIMIT)
                   .WithSize(1,1)
                   .WithPosition(0,0)
                   .GetEntry();
-            m_nte_Bert_FwdLimit = m_sbt_Launcher->AddPersistent("Bert Fwd Limit", ConLauncher::BERT_FWD_LIMIT)
-                  .WithSize(1,1)
-                  .WithPosition(1,0)
-                  .GetEntry();
-            m_nte_Ernie_RevLimit = m_sbt_Launcher->AddPersistent("Ernie Rev Limit", ConLauncher::ERNIE_REV_LIMIT)
+            m_nte_Ernie_Position = m_sbt_Launcher->AddPersistent("Ernie Pos", ConLauncher::ERNIE_REV_LIMIT)
                   .WithSize(1,1)
                   .WithPosition(0,1)
                   .GetEntry();
+
+            // Robot inputs <- Shuffleboard
+            m_nte_Bert_FwdLimit = m_sbt_Launcher->AddPersistent("Bert Fwd Limit", ConLauncher::BERT_FWD_LIMIT)
+                  .WithSize(1,1)
+                  .WithPosition(2,0)
+                  .GetEntry();
+            m_nte_Ernie_FwdLimit = m_sbt_Launcher->AddPersistent("Ernie Fwd Limit", ConLauncher::ERNIE_FWD_LIMIT)
+                  .WithSize(1,1)
+                  .WithPosition(2,1)
+                  .GetEntry();
             m_nte_Bert_RevLimit = m_sbt_Launcher->AddPersistent("Bert Rev Limit", ConLauncher::BERT_REV_LIMIT)
                   .WithSize(1,1)
-                  .WithPosition(1,1)
+                  .WithPosition(3,0)
                   .GetEntry();
-            m_nte_Ernie_Position = m_sbt_Launcher->AddPersistent("Ernie Pos", ConLauncher::ERNIE_REV_LIMIT)
+            m_nte_Ernie_RevLimit = m_sbt_Launcher->AddPersistent("Ernie Rev Limit", ConLauncher::ERNIE_REV_LIMIT)
                   .WithSize(1,1)
-                  .WithPosition(0,2)
-                  .GetEntry();
-            m_nte_Bert_Position = m_sbt_Launcher->AddPersistent("Bert Pos", ConLauncher::BERT_REV_LIMIT)
-                  .WithSize(1,1)
-                  .WithPosition(1,2)
-                  .GetEntry();
-            m_nte_Ernie_Power = m_sbt_Launcher->AddPersistent("Ernie Power", ConLauncher::ERNIE_POWER)
-                  .WithSize(2,1)
-                  .WithPosition(2,0)
+                  .WithPosition(3,1)
                   .GetEntry();
             m_nte_Bert_Power = m_sbt_Launcher->AddPersistent("Bert Power", ConLauncher::BERT_POWER)
                   .WithSize(2,1)
-                  .WithPosition(2,1)
+                  .WithPosition(4,0)
                   .GetEntry();
-
+            m_nte_Ernie_Power = m_sbt_Launcher->AddPersistent("Ernie Power", ConLauncher::ERNIE_POWER)
+                  .WithSize(2,1)
+                  .WithPosition(4,1)
+                  .GetEntry();
             m_ErnieFwdPower = ConLauncher::ERNIE_POWER;
             m_BertFwdPower = ConLauncher::BERT_POWER;
+
+            #ifdef ENABLE_LAUNCHER
             // Ensure the launcher is in the retracted position
             Retract();
+            #endif
           }
 
 void Launcher::Launch() {
+  #ifdef ENABLE_LAUNCHER
   LaunchBert();
   LaunchErnie();
+  #endif
 }
 
 void Launcher::LaunchBert() {
   printf("Launcher::LaunchBert() Executing...\n");  
   // Launch a ball
+  #ifdef ENABLE_LAUNCHER
   m_launcherMotorBert.Set(m_BertFwdPower);
+  #endif
 }
 
 void Launcher::LaunchErnie() {
   printf("Launcher::LaunchErnie() Executing...\n");  
   // Launch a ball
+  #ifdef ENABLE_LAUNCHER
   m_launcherMotorErnie.Set(m_ErnieFwdPower);
+  #endif
 }
 
 void Launcher::Retract() {
   // Bring both launchers back
+  #ifdef ENABLE_LAUNCHER
   RetractBert();
   RetractErnie();
+  #endif
 }
 
 void Launcher::RetractBert() {
   printf("Launcher::RetractBert() Executing...\n");  
+  #ifdef ENABLE_LAUNCHER
   m_launcherMotorBert.Set(-.1);
+  #endif
 }
 
 void Launcher::RetractErnie() {
   printf("Launcher::RetractErnie() Executing...\n");  
+  #ifdef ENABLE_LAUNCHER
   m_launcherMotorErnie.Set(-.1);
-}
-void Launcher::SetLaunchSoftLimit() {
-  m_launcherMotorErnie.SetSoftLimit(rev::CANSparkMax::SoftLimitDirection::kForward, m_nte_Ernie_FwdLimit.GetDouble(115.0));
-  m_launcherMotorErnie.SetSoftLimit(rev::CANSparkMax::SoftLimitDirection::kReverse, m_nte_Ernie_RevLimit.GetDouble(0.0));
-  m_launcherMotorBert.SetSoftLimit(rev::CANSparkMax::SoftLimitDirection::kForward, m_nte_Bert_FwdLimit.GetDouble(115.0));
-  m_launcherMotorBert.SetSoftLimit(rev::CANSparkMax::SoftLimitDirection::kReverse, m_nte_Bert_RevLimit.GetDouble(0.0));
-  m_launcherMotorBert.BurnFlash();
+  #endif
 }
 
-void Launcher::SetResetSoftLimit() {}
+void Launcher::SetLaunchSoftLimits() {
+  #ifdef ENABLE_LAUNCHER
+  m_launcherMotorErnie.SetSoftLimit(rev::CANSparkMax::SoftLimitDirection::kForward, m_nte_Ernie_FwdLimit.GetDouble(ConLauncher::ERNIE_FWD_LIMIT));
+  m_launcherMotorErnie.SetSoftLimit(rev::CANSparkMax::SoftLimitDirection::kReverse, m_nte_Ernie_RevLimit.GetDouble(ConLauncher::ERNIE_REV_LIMIT));
+  m_launcherMotorBert.SetSoftLimit(rev::CANSparkMax::SoftLimitDirection::kForward, m_nte_Bert_FwdLimit.GetDouble(ConLauncher::BERT_FWD_LIMIT));
+  m_launcherMotorBert.SetSoftLimit(rev::CANSparkMax::SoftLimitDirection::kReverse, m_nte_Bert_RevLimit.GetDouble(ConLauncher::BERT_REV_LIMIT));
+  m_launcherMotorBert.BurnFlash();  
+  #endif
+}
+
+void Launcher::SetResetSoftLimits() {
+}
 
 void Launcher::Stop() {
+  #ifdef ENABLE_LAUNCHER
   m_launcherMotorErnie.Set(0.0);
   m_launcherMotorBert.Set(0.0);
+  #endif
 }
 
 void Launcher::Periodic() {
   // Push/Pull stuff from the Network Tables
+  #ifdef ENABLE_LAUNCHER
   m_ErnieFwdPower = m_nte_Ernie_Power.GetDouble(ConLauncher::ERNIE_POWER);
   m_BertFwdPower = m_nte_Bert_Power.GetDouble(ConLauncher::BERT_POWER);
   m_nte_Bert_Position.SetDouble(m_launcherEncoderBert.GetPosition());
   m_nte_Ernie_Position.SetDouble(m_launcherEncoderErnie.GetPosition());
+  #endif
 }
